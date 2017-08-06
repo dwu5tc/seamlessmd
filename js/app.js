00af2d173ed0dbe0a83b2b59167ef09702bcc891
Dean <dean.wu55@gmail.com>
2017-08-06 18:20:39 -0400

    done i think?

--- js/app.js
+++ js/app.js
@@ -8,20 +8,54 @@ $(function() {
 	myApp.init();
 });
 
-myApp.addDisplayPatientListener = () => {
-	$(".patient-data__button").on("click", function() {
-		$.when(myApp.getPatientData())
-		.then(function() {
-			console.log(response);
-		})
-	});
+myApp.parsePatientData = (resp) => {
+	var patient = {};
+	patient.name =  resp.name.given.reduce((a, b) => a + " " + b);
+	patient.name += " " + resp.name.family;
+	patient.organization = resp.managingOrganization.display;
+	patient.gender = resp.gender;
+	patient.conditions = resp.conditions;
+	return patient;
+}
+
+myApp.displayPatientData = (patient) => {
+	$(".patient-data").html(`
+		<p><span>Name of patient:</span> ${patient.name}</p>
+		<p><span>Organization name</span>: ${patient.organization}</p>
+		<p><span>Gender:</span> ${patient.gender}</p>
+		<p><span>Number of conditions they have:</span> ${patient.conditions.length}</p>
+		<p><span>List of all conditions: </span><p>
+		<ul class="patient__conditions"></ul>
+		<button class="patient-data__reset"><span>Back</span></button>
+		`);
+	patient.conditions.forEach((condition) => {
+		$(".patient__conditions").append(`
+			<li>${condition}</li>
+			`);
+	})
 }
 
 myApp.getPatientData = () => {
 	return $.ajax ({
-		url: "ajax/patient.json",
+		url: "http://deanwu.me/seamlessmd/json/patient.json",
 		dataType: "json",
 		type: "GET"
 	});
 }
 
+myApp.addDisplayPatientListener = () => {
+	$(".patient").on("click", ".patient-data__get", () => {
+		$.when(myApp.getPatientData())
+		.then(function(resp) {
+			console.log(resp);
+			myApp.displayPatientData(myApp.parsePatientData(resp));
+		});
+	});
+	$(".patient").on("click", ".patient-data__reset", () => {
+		$(".patient").html(`
+			<div class="patient-data">
+				<button class="patient-data__get"><span>Get Patient Data</span></button>
+			</div>
+		`);
+	});
+}
